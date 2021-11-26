e1fd9fedbcc48b9cb785266aa46c84d9b6ce68e9
tsingkwai <tsingkwai@bluelaketable>
2021-11-26 21:22:02 +0800

    fixed the old folder structure in get mods

--- codes/tools/get.fish
+++ codes/tools/get.fish
@@ -6,7 +6,7 @@ function get -d "get libs from online repo"
     set_color normal
     exit
   end
-  cd fish_libs/libs
+  cd libs
   wget "https://github.com/happyeggchen/progynosh-script-source/raw/main/$modulename/$modulename"
   set_color cyan
   echo "$prefix Processed"
